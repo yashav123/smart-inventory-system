9feb8be51bc6bafb363490af38b8bc82f59f2fa2
Yashav Aggarwal <125884670+yashav123@users.noreply.github.com>
2026-07-06 12:36:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/inventoryDB', {
