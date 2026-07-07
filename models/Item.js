d2480077bb8a35ed02b846bf1fd4f13b50a68d8f
Yashav Aggarwal <125884670+yashav123@users.noreply.github.com>
2026-07-07 10:45:51 +0530

    Update Item.js

--- models/Item.js
+++ models/Item.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const itemSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
-  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+  category: { type: mongoose.Schema.Types.ObjectID},
   price: { type: Number, required: true },
   quantity: { type: Number, default: 0 },
   minQuantity: { type: Number, default: 5 }
